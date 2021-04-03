d5f3b660acc83153a931608b7efff94aa340e3e6
bdlowery <16507857+bdlowery@users.noreply.github.com>
2021-04-02 22:11:42 -0700

    incorporate app.js for features section. Complete features section, complete download section

---
+++ number6/app.js
@@ -0,0 +1,46 @@
+const featureClick = function(buttonClick) {
+let bookmarkButton = document.querySelector('.bookmark-button');
+let searchButton = document.querySelector('.search-button');
+let shareButton = document.querySelector('.share-button');
+
+let featuresBookmark = document.querySelector('.features__bookmark');
+let featuresSearch = document.querySelector('.features__search');
+let featuresShare = document.querySelector('.features__share');
+
+
+
+if (buttonClick == "bookmark") {
+  bookmarkButton.classList.add("features__nav-button--is-active");
+  searchButton.classList.remove("features__nav-button--is-active");
+  shareButton.classList.remove("features__nav-button--is-active");
+
+  featuresBookmark.classList.remove("features__display-none");
+  featuresShare.classList.add("features__display-none");
+  featuresSearch.classList.add("features__display-none");
+} else if (buttonClick == "search") {
+  bookmarkButton.classList.remove("features__nav-button--is-active");
+  searchButton.classList.add("features__nav-button--is-active");
+  shareButton.classList.remove("features__nav-button--is-active");
+
+  featuresBookmark.classList.add("features__display-none");
+  featuresShare.classList.add("features__display-none");
+  featuresSearch.classList.remove("features__display-none");
+} else if (buttonClick == "share") {
+  bookmarkButton.classList.remove("features__nav-button--is-active");
+  searchButton.classList.remove("features__nav-button--is-active");
+  shareButton.classList.add("features__nav-button--is-active");
+
+  featuresBookmark.classList.add("features__display-none");
+  featuresShare.classList.remove("features__display-none");
+  featuresSearch.classList.add("features__display-none");
+} else {
+  bookmarkButton.classList.add("features__nav-button--is-active");
+  searchButton.classList.remove("features__nav-button--is-active");
+  shareButton.classList.remove("features__nav-button--is-active");
+
+  featuresBookmark.classList.remove("features__display-none");
+  featuresShare.classList.add("features__display-none");
+  featuresSearch.classList.add("features__display-none");
+}
+
+}
