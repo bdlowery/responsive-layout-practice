bea30bdb5a691940096e7d3a834308a56bf916eb
bdlowery <16507857+bdlowery@users.noreply.github.com>
2021-04-18 01:15:29 -0700

    polish up basic styles for mobile. fix swiper.js styles for mobile

--- number8/app.js
+++ number8/app.js
@@ -1,13 +1,25 @@
     var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      slidesPerGroup: 3,
+      slidesPerView: 1,
+      spaceBetween: 10,
+      slidesPerGroup: 1,
       grabCursor: true,
       loop: true,
       loopFillGroupWithBlank: true,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+          slidesPerGroup: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          slidesPerGroup: 3,
+        },
       },
       navigation: {
         nextEl: '.swiper-button-next',
