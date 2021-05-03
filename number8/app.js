cb3499780b4ff3282782001421664bf9e38c6306
bdlowery <16507857+bdlowery@users.noreply.github.com>
2021-05-03 17:17:43 -0600

    finish desktop and mobile responsivness. Finish desktop and mobile styles. Finish desktop and mobile navbar. Footer Work in progress.

--- number8/app.js
+++ number8/app.js
@@ -1,28 +1,45 @@
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      slidesPerGroup: 1,
-      grabCursor: true,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-          slidesPerGroup: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-          slidesPerGroup: 3,
-        },
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  slidesPerGroup: 1,
+  grabCursor: true,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      slidesPerGroup: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Look for .hamburger
+let hamburger = document.querySelector(".hamburger");
+
+let navList = document.querySelector('.nav__list-primary');
+
+let html = document.querySelector("html")
+
+// On click
+hamburger.addEventListener("click", function () {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+  navList.classList.toggle("active");
+  html.classList.toggle("background-clicked")
+});
+
