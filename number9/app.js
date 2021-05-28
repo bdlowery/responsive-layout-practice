8fcd71cc6bc3f521514aad05e38db3235fd17eb7
bdlowery <16507857+bdlowery@users.noreply.github.com>
2021-05-27 21:04:52 -0600

    finish mobile nav, start mobile hero view, start basic desktop nav

--- number9/app.js
+++ number9/app.js
@@ -1,6 +1,8 @@
 let hamburger = document.querySelector(".hamburger");
 
-// let navList = document.querySelector('.nav__list-primary');
+let navList = document.querySelectorAll(".nav-list");
+
+let navContainerFlex = document.querySelector(".nav-container-flex");
 
 // let html = document.querySelector("html")
 
@@ -9,6 +11,11 @@ hamburger.addEventListener("click", function () {
   // Toggle class "is-active"
   hamburger.classList.toggle("is-active");
   // Do something else, like open/close menu
-  navList.classList.toggle("active");
+
+  navContainerFlex.classList.toggle('active');
+
+  // for (var i = 0, navListLength = navList.length; i < navListLength; i++) {
+  //   navList[i].classList.toggle("active");
+  // }
   // html.classList.toggle("background-clicked")
 });
