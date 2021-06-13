a4f4966dada1010ed68fc403e239e2bc73203bfd
bdlowery <16507857+bdlowery@users.noreply.github.com>
2021-06-13 16:39:17 -0600

    basic implementation of  fetch api -- able to submit url to shrtco.de api, get a short url back, and display original with short url.

--- number9/app.js
+++ number9/app.js
@@ -12,10 +12,60 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
   // Do something else, like open/close menu
 
-  navContainerFlex.classList.toggle('active');
+  navContainerFlex.classList.toggle("active");
 
   // for (var i = 0, navListLength = navList.length; i < navListLength; i++) {
   //   navList[i].classList.toggle("active");
   // }
   // html.classList.toggle("background-clicked")
 });
+
+//shrtco.de api
+
+let shortenUrl = "https://api.shrtco.de/v2/shorten?url=";
+let shortenLinkForm = document.querySelector(".shorten-link-form");
+let shortenLinkInput = document.getElementById("shorten-link");
+let outputContainer = document.querySelector(".output-container");
+
+shortenLinkForm.addEventListener("submit", async function (buttonClick) {
+  buttonClick.preventDefault();
+  let originalUrl = shortenLinkInput.value;
+  console.log(originalUrl);
+
+  fetch(`https://api.shrtco.de/v2/shorten?url=${originalUrl}`)
+    .then((apiResponse) => apiResponse.json())
+    .then((apiData) => renderOutput(originalUrl, apiData.result.full_short_link));
+});
+
+// 
+
+function renderOutput(userInputLink, shortenedLink) {
+
+  let outputContent = `<div class="output-api-data">
+  <div class="output-api-flex">
+    <div class="original-url">
+      <p class="original-url-text">${userInputLink}</p>
+    </div>
+    <div class="shortened-url shortened-url-flex">
+      <p class="shortened-url-text">${shortenedLink}</p>
+      <button type="submit" class="copy-button">Copy</button>
+    </div>
+  </div>
+</div>`
+  console.log(userInputLink, shortenedLink);
+
+  outputContainer.insertAdjacentHTML("afterbegin", outputContent);
+}
+
+// shortenLinkForm.addEventListener("submit", async function(buttonClick) {
+//   buttonClick.preventDefault();
+//   try {
+//     let apiResponse = await fetch(shortenUrl, {
+//       method: 'POST',
+
+//     })
+//   }
+//   catch {
+//   console.log("button works")
+// }
+// })
